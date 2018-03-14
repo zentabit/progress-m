349cda356891b6609d0e2b78aeccc2983e93fdbd
Emil Babayev <emil.babayev@itggot.se>
2018-03-14 11:05:20 +0100

    First commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,12 @@
+class HomeController < ApplicationController
+  require 'Time'
+  require 'Date'
+  def index
+    time_start = Date.new(2016,8,22).to_time.to_f
+    time_now = Time.now.to_i - time_start
+    # time_now = Date.new(2018,6,10).to_time.to_i - time_start
+    time_end = Date.new(2019,6,10).to_time.to_i - time_start
+    @percent = ((time_now/time_end).round(2)*100).to_i
+    @pixels = @percent * 4
+  end
+end
